f8de4339bda1c19809d3231df76676fb03bb07ce
Tarek Ali <tarekali15@outlook.com>
2020-08-28 18:30:55 -0500

    finished main.py (pretest may change)

--- api-client/main.py
+++ api-client/main.py
@@ -8,25 +8,26 @@ with open('key.json') as f:
 
 
 configuration = {
-   "key": bot_settings["openweather"],
-   "city": bot_settings["city"] 
+    "key": bot_settings["openweather"],
+    "city": bot_settings["city"],
+    "filename": "data.csv"
 }
 
-response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={configuration['city']}&appid={configuration['key'}")
+response = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={configuration['city']}&appid={configuration['key']}")
 
 fields = ['temp', 'humidity', 'pressure', 'weather']
 
 if response.status_code == 200:
-   try: 
-      with open('data.csv') as c:
-         csvwriter = csv.writer(c)
-         csvwriter.writerow(fields)
-
-      data = response.json()
-      main = data['main']
-    # temperatureK = main['temp']
-    # humidity = main['humidity']
-    # pressure = main['pressure']
-    # report = data['weather']
-   except:
-      ...
+    try:
+        with open(configuration["filename"]) as c:
+            csvwriter = csv.writer(c)
+            csvwriter.writerow(fields)
+            data = response.json()
+            main = data['main']
+            rows = [
+                [main['temp'], main['humidity'], main['pressure'], data['weather']]
+            ]
+            csvwriter.writerows(rows)
+    except ValueError as e:
+        print(f"Error: {e}")
