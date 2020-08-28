98efd1f7299d872d23bb9459fb36060ca9efa3a9
Tarek Ali <tarekali15@outlook.com>
2020-08-28 18:00:50 -0500

    uncommented unnecessary things

--- api-client/main.py
+++ api-client/main.py
@@ -10,26 +10,19 @@ key = bot_settings["openweather"]
 city = "Houston"
 
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}"
-# HTTP request
+
 response = requests.get(url)
-# checking the status code of the request
+
 if response.status_code == 200:
-   # getting data in the json format
    data = response.json()
-   # getting the main dict block
    main = data['main']
-   # getting temperature
-   temperatureK = main['temp']
-   temperatureC = temperatureK - 273.15
-   temperatureF = temperatureC*(9/5) + 32
-   # getting the humidity
-   humidity = main['humidity']
-   # getting the pressure
-   pressure = main['pressure']
-   # weather report
-   report = data['weather']
+   # temperatureK = main['temp']
+   # temperatureC = temperatureK - 273.15
+   # temperatureF = temperatureC*(9/5) + 32
+   # humidity = main['humidity']
+   # pressure = main['pressure']
+   # report = data['weather']
 else:
-   # showing the error message
    print("Error in the HTTP request")
 
    
