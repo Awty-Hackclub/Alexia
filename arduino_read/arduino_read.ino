5dfd560147cb4965e7fc396a439f33c760153ca4
gallo-json <gallojsantiago@gmail.com>
2020-08-29 16:28:39 -0500

    final :(

--- arduino_read/arduino_read.ino
+++ arduino_read/arduino_read.ino
@@ -1,11 +1,13 @@
 void setup() {
   Serial.begin(9600); // set the baud rate
+  delay(10000);
 }
 char incoming;
+
 void loop() {
   if(Serial.available() > 0){ // only send data back if data has been sent
     incoming = Serial.read(); // read the incoming data
-    Serial.println(incoming); // send the data back in a new line so that it is not all one long line
+    Serial.print(incoming); // send the data back in a new line so that it is not all one long line
   }
 
 }
