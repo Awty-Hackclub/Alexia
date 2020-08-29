322c7f4620b18af1e945fefc5111a99958961e15
Tarek Ali <tarekali15@outlook.com>
2020-08-29 11:56:58 -0500

    more csv fields

--- api-client/main.py
+++ api-client/main.py
@@ -17,20 +17,23 @@ configuration = {
 response = requests.get(
     f"https://api.openweathermap.org/data/2.5/weather?q={configuration['city']}&appid={configuration['key']}")
 
-fields = ['temp', 'humidity', 'pressure', 'weather']
+fields = ['temp', 'humidity', 'pressure',
+          'weather', 'confirmed', 'deaths', 'recovered']
 while True:
     if response.status_code == 200:
         try:
             with open(configuration["filename"], "w+") as c:
+                covid_data = count(key["country"])
                 csvwriter = csv.writer(c)
                 csvwriter.writerow(fields)
                 data = response.json()
                 main = data['main']
                 rows = [
-                    [main['temp'], main['humidity'], main['pressure'], data['weather'], ]
+                    [main['temp'], main['humidity'],
+                        main['pressure'], data['weather'], covid_data['confirmed'], covid_data['deaths'], covid_data['recovered']]
                 ]
                 csvwriter.writerows(rows)
         except ValueError as e:
             print(f"Error: {e}")
             break
-    time.sleep(600) # every 10 minutes
+    time.sleep(600)  # every 10 minutes
